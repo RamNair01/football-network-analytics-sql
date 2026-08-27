519cc413236c5863a432310b4f8536a75e08c354
Ramnarain Nair <ramnarainnair@gmail.com>
2026-08-27 12:00:00 +0200

    Clean up

--- sql/03_raw_data_quality_checks.sql
+++ sql/03_raw_data_quality_checks.sql
@@ -1,96 +1,73 @@
 -- Just a regular check on the number of rows in each table
-SELECT 'matches' AS table_name, COUNT(*) AS row_count
+SELECT 'matches' AS table_name,
+    COUNT(*)
 FROM raw.matches
-
 UNION ALL
-
-SELECT 'events', COUNT(*)
+SELECT 'events',
+    COUNT(*)
 FROM raw.events
-
 UNION ALL
-
-SELECT 'clubelo_ratings', COUNT(*)
+SELECT 'clubelo_ratings',
+    COUNT(*)
 FROM raw.clubelo_ratings
-
 UNION ALL
-
-SELECT 'non_league_fixtures', COUNT(*)
+SELECT 'non_league_fixtures',
+    COUNT(*)
 FROM raw.non_league_fixtures
-
 UNION ALL
-
-SELECT 'team_name_mapping', COUNT(*)
+SELECT 'team_name_mapping',
+    COUNT(*)
 FROM raw.team_name_mapping;
-
 /* Check for duplicate rows in the events tables, in
-case something went wrong with the data ingestion.
+ case something went wrong with the data ingestion.
  */
-SELECT
-    match_id, event_id, COUNT(*)
-FROM
-    raw.events
-GROUP BY
-    match_id, event_id
-HAVING 
-    COUNT(*) > 1
-
-/* Counting if all events have a match_id that exists 
-in the matches table. 
-*/
-
-SELECT
-    COUNT(*) AS events_with_no_match
-FROM
-    raw.events e
-LEFT JOIN
-    raw.matches m
-ON
-    e.match_id = m.match_id
-WHERE
-    m.match_id IS NULL;
-
+SELECT match_id,
+    event_id,
+    COUNT(*)
+FROM raw.events
+GROUP BY match_id,
+    event_id
+HAVING COUNT(*) > 1
+    /* Counting if all events have a match_id that exists 
+     in the matches table. 
+     */
+SELECT COUNT(*) AS events_with_no_match
+FROM raw.events e
+    LEFT JOIN raw.matches m ON e.match_id = m.match_id
+WHERE m.match_id IS NULL;
 /* Checking that the unique number of matches in the events table
-is the same as the number of matches in the matches table. 
-*/
-SELECT
-    DISTINCT match_id FROM raw.events
+ is the same as the number of matches in the matches table. 
+ */
+SELECT DISTINCT match_id
+FROM raw.events
 EXCEPT -- NOT FOR SELF: https://www.geeksforgeeks.org/sql/sql-except-clause/
-SELECT
-    match_id FROM raw.matches;
-
+SELECT match_id
+FROM raw.matches;
 /* I want to check the team name mismatch 
-between the matches table and the clubelo ratings table.
-This is important because I want to standadize the team names
-across the tables when I add the clubelo ratings to my panel data.
-*/
+ between the matches table and the clubelo ratings table.
+ This is important because I want to standadize the team names
+ across the tables when I add the clubelo ratings to my panel data.
+ */
 SELECT COUNT(*) AS home_team_names_missing_from_clubelo
 FROM (
-    SELECT DISTINCT home_team_name
-    FROM raw.matches
-
-    EXCEPT
-
-    SELECT DISTINCT clubelo_team_name
-    FROM raw.clubelo_ratings
-) AS missing_teams;
-
-
+        SELECT DISTINCT home_team_name
+        FROM raw.matches
+        EXCEPT
+        SELECT DISTINCT clubelo_team_name
+        FROM raw.clubelo_ratings
+    ) AS missing_teams;
 /* 
-team_name_mapping contains the statsbomb team name and the corresponding
-clubelo team name. I want to check if the clubeelo team names
-are present in the clubelo ratings table.
-*/
+ team_name_mapping contains the statsbomb team name and the corresponding
+ clubelo team name. I want to check if the clubeelo team names
+ are present in the clubelo ratings table.
+ */
 SELECT COUNT(*) AS mapped_teams_missing_from_clubelo
 FROM (
-    SELECT
-        competition_name,
-        clubelo_team_name
-    FROM raw.team_name_mapping
-
-    EXCEPT
-
-    SELECT
-        competition_name,
-        clubelo_team_name
-    FROM raw.clubelo_ratings
-) AS missing_elo_histories;
+        SELECT competition_name,
+            clubelo_team_name
+        FROM raw.team_name_mapping
+        EXCEPT
+        SELECT competition_name,
+            clubelo_team_name
+        FROM raw.clubelo_ratings
+    ) AS missing_elo_histories;
